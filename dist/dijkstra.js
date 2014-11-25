a118dbcc63d9dd48aad1bba8acd14adbe9c565c3
Samar Panda <samar@stepout.com>
2014-11-25 07:51:16 +0530

    Added distributed version

--- dist/dijkstra.js
+++ dist/dijkstra.js
@@ -1,15 +1,53 @@
 (function(global){
+
+	/**
+	* Extract keys
+	*/
 	var extractKeys = function(obj){
 		var keys = [], key;
 		for(key in obj){
-			if(obj.hasOwnProperty(key)){
-				keys.push(key);
-			}
+			// if(obj.hasOwnProperty(key)){
+			// 	keys.push(key);
+			// }
+			Object.prototype.hasOwnProperty.call(obj, key) && keys.push(key);
 		}
 		return keys;
 	};
-	var dijkstra = {
 
+	var sorter = function(a, b){
+		return parseFloat(a) - parseFloat(b);
 	};
-	global.dijkstra = dijkstra;
+
+	var findPaths = function(map, start, end, infinity){};
+
+	var extractShortest = function(predecessors, end){};
+
+	var findShortestPath = function(map, nodes){};
+
+	var toArray = function(list, offset){
+		return Array.prototype.slice.call(list, offset);
+	};
+
+	/**
+	* Constructor function
+	*/
+	var Dijkstra = function(map){
+		this.map = map;
+	};
+
+	/**
+	* Will be accessible by creating new Object of Dijkstra
+	*/
+	Dijkstra.prototype.findShortestPath = function(start, end){
+		return 2;
+	};
+
+	/**
+	* Will be accessible without creating new Object of Dijkstra
+	*/
+	Dijkstra.findShortestPath = function(map, start, end){
+		return 1;
+	};
+
+	global.Dijkstra = Dijkstra;
 })(window);
