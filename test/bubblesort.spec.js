183a4d1376107f31ce3ce665220301a7849bdd65
Samar Panda <samar.panda84@gmail.com>
2015-03-15 10:11:35 +0530

    more test cases added for bubblesort

--- test/bubblesort.spec.js
+++ test/bubblesort.spec.js
@@ -9,10 +9,39 @@ describe('BubbleSort', function(){
 		expect(BubbleSort).toBeDefined();
 	});
 
-	it('BubbleSort prototype', function(){
-		var arr = [3, 2, 10, 6, 16];
-		var sp = BubbleSort(arr);
-		expect(sp).toEqual([2, 3, 6, 10, 16]);
+	it('Should work with empty array', function(){
+		expect(BubbleSort([])).toEqual([]);
 	});
-});
 
+	it('Should work for sorted array', function(){
+		expect(BubbleSort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+	});
+
+	it('BubbleSort with random array', function(){
+		var arr = createRandomArray();
+		expect(BubbleSort(arr)).toEqual(arr.sort());
+	});
+
+	it('Descending order BubbleSort with random array', function(){
+		var arr = createRandomArray();
+		expect(BubbleSort(arr, comparator)).toEqual(arr.sort(comparator));
+	});
+
+
+	function comparator(a, b){
+		return b - a;
+	}
+
+	function createRandomArray(config){
+		config = config || {};
+		var size = config.size || 10;
+		var multiplier = config.multiplier || 100;
+		var precesion = config.precesion || 0;
+		var result = [];
+
+		for(var i = size; i>0; i -= 1){
+			result.push(parseFloat(Math.random() * multiplier).toFixed(precesion));
+		}
+		return result;
+	}
+});
