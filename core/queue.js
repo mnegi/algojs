ac1b06e5eab40f90a0513441b14ca30d0e33325c
Samar Panda <samar.panda84@gmail.com>
2015-07-15 19:44:00 +0530

    Added a queue

--- core/queue.js
+++ core/queue.js
@@ -1,25 +1,40 @@
 function Queue() {
 
+	// Initialize the queue and the offset
 	var queue = [];
 	var offset = 0;
 
+	// Retuens of the length of the queue
 	this.getLength = function(){
-
+		return queue.length - offset;
 	};
 
+	// Returns true if the queue is empty
 	this.isEmpty = function(){
-
+		return queue.length === 0;
 	};
 
-	this.enqueue = function(){
-
+	// Enqueues the item
+	this.enqueue = function(item){
+		queue.push(item);
 	};
 
+	// Dequeues a item and returns it. If empty, undefined is returned.
 	this.dequeue = function(){
+		if(queue.length === 0) return undefined;
 
+		var item = queue[offset];
+
+		if( ++offset * 2 > queue.length){
+			queue = queue.slice(offset);
+			offset = 0;
+		}
+
+		return item;
 	};
 
+	// Returns a item from the front of the queue
 	this.peek = function(){
-
+		return queue.length > 0 ? queue[offset] : undefined;
 	};
 }
