13f045f1dab05674cfbe9fd20d123f945bb2c3db
Samar Panda <samar.panda84@gmail.com>
2015-07-15 08:00:48 +0530

    Stubs added for a queue

---
+++ core/queue.js
@@ -0,0 +1,25 @@
+function Queue() {
+
+	var queue = [];
+	var offset = 0;
+
+	this.getLength = function(){
+
+	};
+
+	this.isEmpty = function(){
+
+	};
+
+	this.enqueue = function(){
+
+	};
+
+	this.dequeue = function(){
+
+	};
+
+	this.peek = function(){
+
+	};
+}
